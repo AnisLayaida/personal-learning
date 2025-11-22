f28bd07d2142ac757f242917a4d48219dd7cf60a
AnisLayaida <nissolay@hotmail.com>
2025-11-22 13:02:10 +0000

    initial

---
+++ software-projects/project-based-learning/course-learning/python-codedex/python-intermediate/module-2-file-i-o/exercise-6.py
@@ -0,0 +1,18 @@
+# File Handling
+
+# File input/output (I/O) handing lets us interact with external files for various purposes, such as:
+# Saving info to a file by writing to it
+# Reading from a file to use its info
+# Maintaining computor file systems
+
+# open() method is your gateway to handle files in python
+# It can create (if it doesnt exist) or open (if it exists)
+
+file = open('example.txt', 'w') # Opens example file for writing
+
+file.write("Hello World!!") # The write() method allows you to write in the file
+
+# Intructions: Use the open() method to create a new diary and write your secrets inside!
+
+file = open('diary.txt', 'w')
+file.write("I'm currently learning python! Im also very hungry lol")
